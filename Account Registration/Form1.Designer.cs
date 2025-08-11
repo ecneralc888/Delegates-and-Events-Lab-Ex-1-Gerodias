3fdcf1b96ebed9320ad3f0011a7b79d395463ce8
ecneralc888 <johnkengerodias05@gmail.com>
2025-08-11 19:47:47 +0800

    Initialcommit

--- Account Registration/Form1.Designer.cs
+++ Account Registration/Form1.Designer.cs
@@ -208,7 +208,7 @@
             this.Controls.Add(this.studentxt);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "FrmRegistration";
             this.ResumeLayout(false);
             this.PerformLayout();
 
